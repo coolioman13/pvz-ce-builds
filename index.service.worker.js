b7bc4a63eca218afd1e47e0a5d8ec4936a9bd85e
coolioman <weegewega@gmail.com>
2026-09-18 14:25:32 +0300

    update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789724987|62118203';
+const CACHE_VERSION = '1789730691|76594689';
 /** @type {string} */
 const CACHE_PREFIX = 'Plants vs. Zombi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
